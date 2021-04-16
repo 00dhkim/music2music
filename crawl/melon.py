453c8d772c4d7c8ee89a4ea82393b39bc0f6171a
Dohyun Kim <00dh.kim@gmail.com>
2021-04-16 13:57:51 +0900

    crawl/melon.py 리팩토링, 로직 오류는 아직 있음

--- crawl/melon.py
+++ crawl/melon.py
@@ -1,41 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 
-drive = webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome("chromedriver.exe")
 
 
-def pageCrawl():
-    trlist = drive.find_elements_by_xpath("//table[@border=\"1\"]/tbody/tr")
-    for tr in trlist:
-        try:
-            song, artist = tr.find_elements_by_xpath("./td[@class=\"t_left\"]")[0:2]
-            song = song.find_element_by_xpath("./div/div/button[@class=\"btn_icon play\"]")
-            title = song.get_attribute('title')
+class MelonCrawler:
+    def __init__(self):
+        self.driver = webdriver.Chrome("chromedriver.exe")
+        self.crawl_data = []
 
-            artist = artist.find_element_by_xpath("./div/div/a")
-            artistName = artist.text
+    def crawl(self):
+        # melon은 로그인 안해도 좋아요 리스트 뜸
+        self.driver.get("https://www.melon.com/mymusic/like/mymusiclikesong_list.htm?memberKey=51230892")
+        while True:
+            try:
+                input()
+                print("crawling")
+                self.pageCrawl()
+                print("end")
+            except:
+                break
 
-            f.write(title.split(" 재생 - 새창")[0] + "|" + artistName + '\n')
-            # print(title.split(" 재생 - 새창")[0]+"|"+artistName)
-        except:
-            print("error")
+        with open("melon_crawl_data.txt", "w", encoding="utf-8") as f:
+            f.write(self.crawl_data.__str__())
+
+        return self.crawl_data
+
+    # 한 페이지만 크롤링함
+    def pageCrawl(self):
+        trlist = self.driver.find_elements_by_xpath("//table[@border=\"1\"]/tbody/tr")
+        for tr in trlist:
+            try:
+                song, artist = tr.find_elements_by_xpath("./td[@class=\"t_left\"]")[0:2]
+                song = song.find_element_by_xpath("./div/div/button[@class=\"btn_icon play\"]")
+                title = song.get_attribute('title')
+
+                artist = artist.find_element_by_xpath("./div/div/a")
+                artistName = artist.text
+
+                music = {'song': song, 'artist': artistName}
+                self.crawl_data.append(music)
+                print(f'crawling...')
+                # f.write(title.split(" 재생 - 새창")[0] + "|" + artistName + '\n')
+                # print(title.split(" 재생 - 새창")[0]+"|"+artistName)
+            except:
+                print("error")
 
 
-# melon은 로그인 안해도 좋아요 리스트 뜸
-drive.get("https://www.melon.com/mymusic/like/mymusiclikesong_list.htm?memberKey=51230892")
-p = pageCrawl
-
-f = open("musicList.txt", "a", encoding="utf-8")
-
-print("init setting end")
-
-while (1):
-    input()
-    print("crawling")
-    p();
-    print("end")
-
-f.close()
+if __name__ == '__main__':
+    melon_crawler = MelonCrawler()
+    melon_crawler.crawl()
 
 '''
 drive.get("http://lms.knu.ac.kr/ilos/main/member/login_form.acl")
